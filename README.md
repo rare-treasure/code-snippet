7cdc404ccd92adc688a58256225cf0c90ae4462c
sifan <v_sfluo@digitalgd.com.cn>
2020-12-30 17:14:24 +0800

    upd: 新增import-vue2，v-tooltip 代码片段

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 > 用于记录日常项目中优秀的代码块
 
-### 代码块
+### 列表
 
-* [echarts-axis](./src/echart-axis.js)，主要用于对echart axis的自动计算，返回interval, min, max等值。用于解决动态数据导致坐标轴部分情况下错乱的问题。
+* [echarts-axis](./src/echart-axis.js)，主要用于对echart axis的自动计算，返回interval, min, max等值。用于解决动态数据导致坐标轴部分情况下错乱的问题（当明确需要图表坐标轴固定行时）。
+
+* [import-vue2](./src/import-vue2.vue)，主要用于处理部分vue2插件，使之能让其直接被vue3使用。
+
+* [v-tooltip](./src/v-tooltip.js)，基于element-plus的Popover组件改造而成的vue指令，仅支持vue3。
---
+++ src/import-vue2.vue
@@ -0,0 +1,18 @@
+<script>
+import { cloneDeep } from 'lodash-es';
+// xxx => 指代vue2 npm组件包。
+import Component from 'xxx';
+
+const CustomComponent = cloneDeep(Component);
+
+CustomComponent.unmounted = CustomComponent.destroyed
+Reflect.deleteProperty(CustomComponent, 'destroyed');
+
+export default {
+  name: 'CustomComponent',
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  render() {
+    return <CustomComponent {...this.$attrs}></CustomComponent>
+  }
+};
+</script>
